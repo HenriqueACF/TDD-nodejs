7291b221211e7591e9a65f3f490cf0d67197a2a7
Henrique Assis <45006908+HenriqueACF@users.noreply.github.com>
2023-04-22 14:21:58 -0300

    feat: preparing environment for request testing

---
+++ src/routes/api.test.ts
@@ -0,0 +1,11 @@
+import request from 'supertest'
+import app from '../app'
+
+describe('Testing api routes', ()=>{
+    it('ping pong', (done)=>{
+        request(app).get('/ping').then(response=>{
+            expect(response.body.pong).toBeTruthy()
+            return done()
+        })
+    })
+})
